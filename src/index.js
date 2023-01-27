167f4f25e624e5295d4f65b881c387eeec61bd90
Christine Belzie <105683440+CBID2@users.noreply.github.com>
2023-01-27 00:00:44 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,73 @@
+const time = document.getElementById("time");
+
+function displayTime() {
+  const currentTime = new Date();
+  let dayOfWeek;
+  switch (currentTime.getDay()) {
+    case 0:
+      dayOfWeek = "Sunday";
+      break;
+    case 1:
+      dayOfWeek = "Monday";
+      break;
+    case 2:
+      dayOfWeek = "Tuesday";
+      break;
+    case 3:
+      dayOfWeek = "Wednesday";
+      break;
+    case 4:
+      dayOfWeek = "Thursday";
+      break;
+    case 5:
+      dayOfWeek = "Friday";
+      break;
+    case 6:
+      dayOfWeek = "Saturday";
+      break;
+    default:
+      dayOfWeek = "Invalid Day of Week";
+      console.log("Invalid Day of Week");
+  }
+  const date = currentTime.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+  });
+  const hour = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  time.textContent = `${dayOfWeek}, ${date} ${hour}:${minutes}`;
+}
+
+setInterval(displayTime, 1000);
+
+const form = document.getElementById("search-form");
+const cityName = document.getElementById("city-name");
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const cityInput = document.querySelector('input[name="q"]');
+  const city = cityInput.value;
+  searchCity(city);
+}
+
+function searchCity(city) {
+  const apiKey = "f853d779bfd20dcdcb8c01930821455a";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  axios.get(apiUrl).then((res) => {
+    showTemperature(res.data.main.temp);
+    showCityName(res.data.name);
+  });
+}
+
+function showTemperature(temperature) {
+  const temperatureElement = document.querySelector("h5");
+  temperatureElement.innerHTML = temperature + "K";
+}
+
+function showCityName(cityName) {
+  const cityNameElement = document.getElementById("city-name");
+  cityNameElement.innerText = cityName;
+}
+
+form.addEventListener("submit", handleSubmit);
