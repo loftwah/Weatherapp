cfde366c63f335eb042712d91273722bd44ec5e9
Christine <shecoder30@gmail.com>
2023-02-07 01:03:51 +0000

    Update 6 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -173,6 +173,7 @@ function searchCity(city) {
     showTemperature(res.data.main.temp);
     showCityName(res.data.name);
   });
+  showWeatherIcon(res.data.weather[0].icon);
 }
 function showTemperature(temperature) {
   var temperatureElement = document.querySelector("h5");
@@ -181,6 +182,10 @@ function showTemperature(temperature) {
 function showCityName(cityName) {
   var cityNameElement = document.getElementById("city-name");
   cityNameElement.innerText = cityName;
+}
+function showWeatherIcon(iconCode) {
+  var iconUrl = "http://openweathermap.org/img/wn/".concat(iconCode, "@2x.png");
+  weatherIcon.setAttribute("src", iconUrl);
 }
 form.addEventListener("submit", handleSubmit);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
